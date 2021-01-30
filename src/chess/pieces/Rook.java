c30c14f2d6e6ba3af9ce72ea7c40eb528b84307b
TomazHaddad <tomato039@outlook.com>
2021-01-30 20:15:35 -0300

    Chess Exception and ChessPosition

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,11 +1,15 @@
 package chess.pieces;
 
+import java.util.Scanner;
+
 import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Rook extends ChessPiece{
 
+	
+	
 	public Rook(Board board, Color color) {
 		super(board, color);
 	}
